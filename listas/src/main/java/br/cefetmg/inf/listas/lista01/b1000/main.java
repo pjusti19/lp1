28f24073c44e8704815007f8415e6454af93de45
paulistaLH <pedrinhojusti@gmail.com>
2023-03-21 23:06:59 -0300

    beecrowd 1000

---
+++ listas/src/main/java/br/cefetmg/inf/listas/lista01/b1000/main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+ 
+public class main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+       System.out.println("Hello World!");
+ 
+    }
+ 
+}
