048df42aa0b3923da8dec8e126e11a996638e1ac
paulistaLH <pedrinhojusti@gmail.com>
2023-03-21 23:12:45 -0300

    beecrowd 1016

--- listas/src/main/java/br/cefetmg/inf/listas/lista01/b1000/main.java
+++ listas/src/main/java/br/cefetmg/inf/listas/lista01/b1000/main.java
@@ -1,10 +1,15 @@
 import java.io.IOException;
- 
+import java.util.Scanner;
+
 public class main {
  
     public static void main(String[] args) throws IOException {
  
-       System.out.println("Hello World!");
+        int x;
+        Scanner input = new Scanner(System.in);
+        x = input.nextInt();
+
+        System.out.printf("%d minutos\n", x*2);
  
     }
  
